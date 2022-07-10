be647d5f84a0f17ece636980c8740b152a80ca60
Jack Davenport <jack.davenport5011@gmail.com>
2022-07-10 16:36:48 +1000

    Implemented invsee.edit permission

--- src/main/java/com/romejanic/invsee/SimpleInvsee.java
+++ src/main/java/com/romejanic/invsee/SimpleInvsee.java
@@ -1,19 +1,23 @@
 package com.romejanic.invsee;
 
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.romejanic.invsee.command.CommandInvsee;
+import com.romejanic.invsee.command.PermissionChecker;
 
 public class SimpleInvsee extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new PermissionChecker(), this);
 		getCommand("invsee").setExecutor(new CommandInvsee());
 		getLogger().info("Started SimpleInvsee!");
 	}
 	
 	@Override
 	public void onDisable() {
+		HandlerList.unregisterAll(this);
 		getLogger().info("Stopped SimpleInvsee. Thanks for using it!");
 	}
 	
---
+++ src/main/java/com/romejanic/invsee/command/PermissionChecker.java
@@ -0,0 +1,36 @@
+package com.romejanic.invsee.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.PlayerInventory;
+
+public class PermissionChecker implements Listener {
+
+	@EventHandler
+	public void inventoryClicked(InventoryClickEvent event) {
+		Player viewer = (Player) event.getWhoClicked();
+		if(hasEditPermission(viewer)) {
+			// if they have edit permission, no need to check what's
+			// going on
+			return;
+		}
+		
+		Inventory inv = event.getInventory();
+		if(inv instanceof PlayerInventory) {
+			PlayerInventory pinv = (PlayerInventory)inv;
+			if(!pinv.getHolder().equals(viewer)) {
+				event.setCancelled(true);
+				viewer.sendMessage(ChatColor.RED + "You don't have permission to edit other player's inventory.");
+			}
+		}
+	}
+	
+	private boolean hasEditPermission(Player player) {
+		return player.isOp() || player.hasPermission("invsee.edit");
+	}
+	
+}
