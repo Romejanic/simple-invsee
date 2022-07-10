922c2be072dc82c8645f695210357a19f7786e96
Jack Davenport <jack.davenport5011@gmail.com>
2022-07-10 15:21:03 +1000

    Added Gradle project

---
+++ src/main/java/com/romejanic/invsee/SimpleInvsee.java
@@ -0,0 +1,17 @@
+package com.romejanic.invsee;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleInvsee extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
